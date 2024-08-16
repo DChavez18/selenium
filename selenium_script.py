49be0b0d0efc60e614d8ce137faf1e4c9ece5ada
Derek Chavez <frrenose17@yahoo.com>
2024-08-16 09:38:49 -0600

    feat: add Select function, and select party size

--- selenium_script.py
+++ selenium_script.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.opentable.com")
@@ -13,14 +14,26 @@ try:
     )
     search_bar.send_keys("Oliver's Italian")
 
+    find_table_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label=\"Let’s go\"]"))
+    )
+
+    find_table_button.click()
+
     restaurant_image = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "img[alt=\"A photo of Oliver's Italian restaurant\"]"))
     )
 
     restaurant_image.click()
 
-    print(driver.current_url)
-    print(driver.title)
+    party_size_dropdown = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.XPATH, "//select[@aria-label='Party size selector']"))
+    )
+    select_party_size = Select(party_size_dropdown)
+    select_party_size.select_by_value("2")
+
+    print("Successfully selected party size")
+
 
 
 finally:
